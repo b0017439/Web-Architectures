f9a89baf0918c04b060c297601e4a85c5eb4a378
Janice Lloyd <janicelloyd09@gmail.com>
2012-04-22 12:51:42 +0100

    Added newly created directories

---
+++ placements/grails-app/domain/placements/Application.groovy
@@ -0,0 +1,12 @@
+package placements
+
+class Application {
+
+ String student
+ String placement
+ String status
+ long timestamp = System.currentTimeMillis()
+
+    static constraints = {
+    }
+}
---
+++ placements/grails-app/domain/placements/PlacementOpportunity.groovy
@@ -0,0 +1,12 @@
+package placements
+
+class PlacementOpportunity {
+
+ String jobTitle
+ String companyName
+ String applications
+ String status
+
+    static constraints = {
+    }
+}
