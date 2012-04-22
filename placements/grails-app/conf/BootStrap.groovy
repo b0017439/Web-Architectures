0ecc73856d5d3f29d92e15952fb167b374833df5
Janice Lloyd <janicelloyd09@gmail.com>
2012-04-22 13:28:12 +0100

    Added default staus data

--- placements/grails-app/conf/BootStrap.groovy
+++ placements/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import placements.Status
 class BootStrap {
 
     def init = { servletContext ->
+        def appliedstatus = new Status(code:'01',description:'Applied').save();
+        def notInvitedstatus = new Status(code:'02',description:'Not invited for Interview').save();
+        def invitedstatus = new Status(code:'03',description:'Invited for Interview').save();
+        def notOfferedtatus = new Status(code:'04',description:'Not Offered').save();
+        def offeredstatus = new Status(code:'05',description:'Offered').save();
+        def acceptedstatus = new Status(code:'06',description:'Accepted').save();
+        def rejectedstatus = new Status(code:'01',description:'Rejected').save();
+
+
     }
     def destroy = {
     }
